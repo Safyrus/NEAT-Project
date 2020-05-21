1d116ebea22e3adecdb3f1c4c82e86456df4926a
Safyrus <54881649+Safyrus@users.noreply.github.com>
2020-05-21 16:29:31 +0200

    Modified to test entities

--- src/main/Canvas.java
+++ src/main/Canvas.java
@@ -1,33 +1,43 @@
 package main;
 
 import java.awt.Graphics;
-import java.awt.Color;
 import javax.swing.JPanel;
 
+import entity.Creature;
+import entity.Food;
 import nn.NeuralNetwork;
+import world.World;
 
 public class Canvas extends JPanel {
     private static final long serialVersionUID = 1L;
 
     private NeuralNetwork nn;
-    private int width;
-    private int height;
+    private World world;
 
-    public Canvas(int width, int height)
+    public Canvas()
     {
         nn = new NeuralNetwork();
+        world = new World(400, 400);
+        for (int i = 0; i < 10; i++) {
+            Food f = new Food();
+            f.setX(Math.random()*world.getW());
+            f.setY(Math.random()*world.getH());
+            world.addEntity(f);
+        }
+        world.addEntity(new Creature());
     }
 
     public void step()
     {
-        nn.step();
-        nn.mutate();
+        //nn.step();
+        //nn.mutate();
+        world.step();
     }
 
     public void paint(Graphics g) {
         super.paint(g);
-        g.setColor(new Color(0, 100, 100));
-        g.clearRect(0, 0, width, height);
-        nn.display(g);
+
+        world.display(g);
+        //nn.display(g);
     }
 }
