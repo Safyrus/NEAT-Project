7428e072eecec41321382bc6c14c64b9b63a70b1
Safyrus <54881649+Safyrus@users.noreply.github.com>
2020-05-21 16:28:31 +0200

    added fps

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,19 +7,20 @@ public class Main {
         JFrame fenetre = new JFrame();
 
         fenetre.setTitle("Test");
-        fenetre.setSize(400, 400);
-        Canvas canvas = new Canvas(fenetre.getWidth(), fenetre.getHeight());
+        fenetre.setSize(640, 480);
+        Canvas canvas = new Canvas();
         fenetre.setLocationRelativeTo(null);
         fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fenetre.setContentPane(canvas);
         fenetre.setVisible(true);
         
         boolean stop = false;
+        int fps = 20;
 
         while (!stop) {
             canvas.step();
             canvas.repaint();
-            Thread.sleep(50);
+            Thread.sleep(1000/fps);
         }
     }
 
